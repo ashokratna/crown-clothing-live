9a840795b0ad872f8beca75857d6ed1cc41d0697
Ankur Pandey <ap622098@gmail.com>
2021-05-17 13:50:25 +0530

    click fire on logo

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,46 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectCurrentUser } from "../../redux/user/user.selectos";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
-import { auth } from '../../firebase/firebase.utils';
-import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from "./header.style";
-import './header.style.css';
+import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink,
+} from "./header.style";
+import "./header.style.css";
 
-const Header = ({ currentUser, hidden }) => (
-  <HeaderContainer>
-    <LogoContainer to='/crown-clothing-live'>
-      <Logo className='logo' />
-    </LogoContainer>
-    <OptionsContainer>
-      <OptionLink to='/shop'>
-        SHOP
-      </OptionLink>
-      <OptionLink to='/shop'>
-        CONTACT
-      </OptionLink>
-      {currentUser ? (
-        <OptionLink as='div' onClick={() => auth.signOut()}>
-          SIGN OUT
-        </OptionLink>
-      ) : (
-        <OptionLink to='/signin'>
-          SIGN IN
-        </OptionLink>
-      )}
-      <CartIcon />
-    </OptionsContainer>
-    { hidden ? null : <CartDropdown /> }
-  </HeaderContainer>
-);
+function Header({ currentUser, hidden }) {
+  function simulateClick(e) {
+    e.click();
+  }
+
+  return (
+    <HeaderContainer>
+      <LogoContainer
+        to='/crown-clothing-live'
+        ref={simulateClick}
+        onClick={() => console.log("clicked")}
+      >
+        <Logo className='logo' />
+      </LogoContainer>
+      <OptionsContainer>
+        <OptionLink to='/shop'>SHOP</OptionLink>
+        <OptionLink to='/shop'>CONTACT</OptionLink>
+        {currentUser ? (
+          <OptionLink as='div' onClick={() => auth.signOut()}>
+            SIGN OUT
+          </OptionLink>
+        ) : (
+          <OptionLink to='/signin'>SIGN IN</OptionLink>
+        )}
+        <CartIcon />
+      </OptionsContainer>
+      {hidden ? null : <CartDropdown />}
+    </HeaderContainer>
+  );
+}
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  hidden: selectCartHidden
+  hidden: selectCartHidden,
 });
 
 export default connect(mapStateToProps)(Header);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,38 +1,27 @@
-import React,{Component} from 'react';
-import './homepage.style.css';
-import { HomePageContainer } from './homepage.style'
-import Directory from '../../components/directory/directory.component';
+import React, { Component } from "react";
+import "./homepage.style.css";
+import { HomePageContainer } from "./homepage.style";
+import Directory from "../../components/directory/directory.component";
 
 var pathName;
 class HomePage extends Component {
-    
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
 
-        pathName  = props.history.location.pathname;
-        
-    }
+    pathName = props.history.location.pathname;
+  }
 
-    componentDidMount(){
-        pathName.replace(/\/$/, "");
-    }
+  componentDidMount() {
+    pathName.replace(/\/$/, "");
+  }
 
-    render() { 
-        return ( 
-            <HomePageContainer>
-                <Directory />
-            </HomePageContainer>
-         );
-    }
+  render() {
+    return (
+      <HomePageContainer>
+        <Directory />
+      </HomePageContainer>
+    );
+  }
 }
- 
-export default HomePage;
 
-// const HomePage = (props) => {
-//     console.log(props)
-//     return ( 
-        
-//      );
-// }
- 
-// export default HomePage;
+export default HomePage;
